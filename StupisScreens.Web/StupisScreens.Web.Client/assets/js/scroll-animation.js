911d1d138e951a0612355ef6ae469ea31c95fb4e
Shibly <shibly77@outlook.com>
2018-04-02 04:41:37 -0700

    New ui start

--- StupisScreens.Web/StupisScreens.Web.Client/assets/js/scroll-animation.js
+++ StupisScreens.Web/StupisScreens.Web.Client/assets/js/scroll-animation.js
@@ -1,46 +1,46 @@
 $(document).ready(function(){
 
-    window.sr = ScrollReveal({ reset: true });
+    // window.sr = ScrollReveal({ reset: true });
    
-    //Animating whats is section
-    animateOnScroll('.whats-title', 600, 'left', '30px', 1, 150);
-    animateOnScroll('.whats-des', 600, 'left', '20px', 1, 150);
+    // //Animating whats is section
+    // animateOnScroll('.whats-title', 600, 'left', '30px', 1, 150);
+    // animateOnScroll('.whats-des', 600, 'left', '20px', 1, 150);
 
-    //Animating features section
-    animateOnScroll('.features-title', 600, 'right', '20px', 1, 150);
-    animateOnScroll('.features-des', 600, 'right', '20px', 1, 150);
+    // //Animating features section
+    // animateOnScroll('.features-title', 600, 'right', '20px', 1, 150);
+    // animateOnScroll('.features-des', 600, 'right', '20px', 1, 150);
     
-    //Create feature section
-    // animateOnScroll('.ftr-create', 600, 'bottom', '20px', 1, 150);
-    // animateOnScroll('.ftr-create', 600, 'bottom', '30px', 1, 150);
-    // animateOnScroll('.ftr-create', 600, 'bottom', '40px', 1, 150);
+    // //Create feature section
+    // // animateOnScroll('.ftr-create', 600, 'bottom', '20px', 1, 150);
+    // // animateOnScroll('.ftr-create', 600, 'bottom', '30px', 1, 150);
+    // // animateOnScroll('.ftr-create', 600, 'bottom', '40px', 1, 150);
 
-    sr.reveal('.ftr-create', { duration: 800 , scale:1 }, 150);
-    sr.reveal('.ftr-edit', { duration: 800, scale:1 }, 150);
-    sr.reveal('.ftr-download', { duration: 800, scale:1 }, 150);
+    // sr.reveal('.ftr-create', { duration: 800 , scale:1 }, 150);
+    // sr.reveal('.ftr-edit', { duration: 800, scale:1 }, 150);
+    // sr.reveal('.ftr-download', { duration: 800, scale:1 }, 150);
 
 
-    sr.reveal('.showkase', { duration: 800, scale : 1, origin : 'left' }, 150);
-    sr.reveal('.showkase-viewer', { duration: 800, scale : 1, origin : 'bottom', distance: '10px' }, 150);
-
-    
+    // sr.reveal('.showkase', { duration: 800, scale : 1, origin : 'left' }, 150);
+    // sr.reveal('.showkase-viewer', { duration: 800, scale : 1, origin : 'bottom', distance: '10px' }, 150);
 
     
-    sr.reveal('.feedback', { duration: 800, scale : 1, origin : 'left' }, 150);
-    animateOnScroll('.feedback-btn', 800, 'bottom', '20px', 1, 300);
+
+    
+    // sr.reveal('.feedback', { duration: 800, scale : 1, origin : 'left' }, 150);
+    // animateOnScroll('.feedback-btn', 800, 'bottom', '20px', 1, 300);
 
 
-    function animateOnScroll (elem, duration, origin, distance, scale, delay)
-    {
-        sr.reveal( elem, 
-        { 
-            duration: duration, 
-            origin: origin, 
-            distance: distance,
-            scale: scale,
-            delay: delay, 
-        });
-    }
+    // function animateOnScroll (elem, duration, origin, distance, scale, delay)
+    // {
+    //     sr.reveal( elem, 
+    //     { 
+    //         duration: duration, 
+    //         origin: origin, 
+    //         distance: distance,
+    //         scale: scale,
+    //         delay: delay, 
+    //     });
+    // }
 });
 
 
